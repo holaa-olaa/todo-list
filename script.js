236b9768f29359f27f27aad2f36d123a324e4c5b
Aleksandra Josko <olajosko41@gmail.com>
2021-05-18 22:30:08 +0200

    adding, removing tasks and task done added

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 {
-    const tasks = [
-        {
-            content: "Nie zjadłam nic",
-            done: true,
-        },
-        {
-            content: "Uczyłam się",
-            done: false,
-        },
-    ];
+    const tasks = [];
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
-        
+
         render();
     };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const taskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const doneButtons = document.querySelectorAll(".js-done");
+
+        doneButtons.forEach((doneButton, index) => {
+            doneButton.addEventListener("click", () => {
+                taskDone(index);
+            });
+        });
+    }
 
     const render = () => {
         let htmlString = "";
@@ -26,13 +45,15 @@
             <li 
                 ${task.done ? " style = \"text-decoration: line-through\"" : ""} 
             >
+                <button class="js-done">zrobione</button>
+                <button class="js-remove">usuń</button>
                 ${task.content}
             </li>
             `
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
+    };
 
 
     const onFormSubmit = (event) => {
@@ -42,14 +63,13 @@
 
         if (newTaskContent === "") {
             return;
-        }         
-            addNewTask(newTaskContent);
-        
+        }
+        addNewTask(newTaskContent);
+
     };
 
-
     const init = () => {
-        render ();
+        render();
 
         const form = document.querySelector(".js-form");
 
